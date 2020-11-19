5cc63ff0f850cd80d0dd1d11984777712209e202
dobrelka <dobrelyatest@gmail.com>
2020-11-19 17:45:54 +0200

    d

--- app/src/androidTest/java/com/raywenderlich/codingcompanionfinder/FindCompanionInstrumentedTest.kt
+++ app/src/androidTest/java/com/raywenderlich/codingcompanionfinder/FindCompanionInstrumentedTest.kt
@@ -164,6 +164,15 @@ class FindCompanionInstrumentedTest {
     onView(withId(R.id.noResults)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
   }
 
+  @Test
+  fun searching_for_a_companion_in_a_call_returns_an_error_displays_no_results() {
+    onView(withId(R.id.searchForCompanionFragment)).perform(click())
+    onView(withId(R.id.searchFieldText)).perform(typeText("dddd"))
+    onView(withId(R.id.searchButton)).perform(click())
+    onView(withId(R.id.searchButton)).check(matches(isDisplayed()))
+    onView(withId(R.id.noResults)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+  }
+
   private fun find_and_select_kevin_in_30318(){
     onView(withId(R.id.searchForCompanionFragment)).perform(click())
     onView(withId(R.id.searchFieldText)).perform(typeText("30318"))
