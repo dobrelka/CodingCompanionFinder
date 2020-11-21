eb5b5620681b878c8e4d5f985f81e72a900d7e2d
dobrelka <dobrelyatest@gmail.com>
2020-11-21 17:09:58 +0200

    Changing class SearchForCompanionViewModel in SearchForCompanionViewModel.kt

--- app/src/main/java/com/raywenderlich/codingcompanionfinder/searchforcompanion/SearchForCompanionViewModel.kt
+++ app/src/main/java/com/raywenderlich/codingcompanionfinder/searchforcompanion/SearchForCompanionViewModel.kt
@@ -16,5 +16,37 @@ class SearchForCompanionViewModel: ViewModel() {
   val noResultsViewVisiblity : MutableLiveData<Int> = MutableLiveData<Int>()
   val companionLocation : MutableLiveData<String> = MutableLiveData()
 
+  val animals: MutableLiveData<ArrayList<Animal>> = MutableLiveData<ArrayList<Animal>>()
+  lateinit var accessToken: String
+  lateinit var petFinderService: PetFinderService
 
+  fun searchForCompanions() {
+
+    GlobalScope.launch {
+      EventBus.getDefault().post(IdlingEntity(1))
+
+      val getAnimalRequest = petFinderService.getAnimals(accessToken, location = companionLocation.value)
+
+      val searchForPetResponse = getAnimalRequest.await()
+
+      GlobalScope.launch(Dispatchers.Main) {
+        if (searchForPetResponse.isSuccessful) {
+          searchForPetResponse.body()?.let {
+
+            animals.postValue(it.animals)
+            if (it.animals.size > 0) {
+
+              noResultsViewVisiblity.postValue(INVISIBLE)
+            } else {
+
+              noResultsViewVisiblity.postValue(View.VISIBLE)
+            }
+          }
+        } else {
+          noResultsViewVisiblity.postValue(View.VISIBLE)
+        }
+      }
+      EventBus.getDefault().post(IdlingEntity(-1))
+    }
+  }
 }
